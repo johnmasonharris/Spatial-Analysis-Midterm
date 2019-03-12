36a39f8dbf891fd81917c7bdb9aec3d3faf843fb
johnmasonharris <john.mason.harris@gmail.com>
2019-03-12 15:24:35 -0500

    macrozooplankton finished and exported as tab delimited file

--- process_student.R
+++ process_student.R
@@ -2,21 +2,37 @@ library(stringr)
 library(plyr)
 library(dplyr)
 library(lubridate)
+library("pastecs")
+library("oce")
+library(tidyverse)
+library(readxl)
+library(tidyr)
 
 # Macrozooplankton data ----
 #read in  data
-z <- read.csv()
+z <- read.csv(file = "195101-201404_Zoop.csv")
+z = as.data.frame(z)
 z <- z[,which(unlist(lapply(z, function(x)!all(is.na(x)))))] #using the "lapply" function from the "dplyr" package, remove fields which contain all "NA" values
 
-#create new fields with decimal degree latitude and longitude values
-z$Lat_DecDeg <-
+#convert latitude to decimal format
+z$Lat_DecMin <- (z$Lat_Min/60)
+z$lat = (z$Lat_Deg + z$Lat_DecMin)
+# repeat for longitude
+z$Lon_DecMin = (z$Lon_Min/60)
+z$lon = (z$Lon_Deg + z$Lon_DecMin) * -1
 
 # create a date-time field
-z$dateTime <- str_c(z$tow_date," ",z$tow_time,":00")
-z$dateTime <- as.POSIXct(strptime(z$dateTime, tz = "America/Los_Angeles")) #Hint: look up input time formats for the 'strptime' function
+z$dateTime <- str_c(z$Tow_Date, " ", z$Tow_Time, ":00")
+z$dateTime <- as.POSIXct(strptime(z$dateTime, "%m/%d/%Y %H:%M:%S", tz = "America/Los_Angeles")) #Hint: look up input time formats for the 'strptime' function
 z$tow_date <- NULL; z$tow_time <- NULL
 
+# keep only certain columns
+z <- z[,c("ID", "Cruise","CruiseAlias", "lat", "lon", "Sta_ID", "Cruz_Sta", "Ship_Code", "Order_Occ", "Cruz_Code", "St_Line", "St_Station", 
+          "Tow_Type", "Net_Loc", "End_Time", "Vol_StrM3", "Tow_DpthM", "Ttl_PVolC3", "Sml_PVolC3", "HaulFact", "dateTime")]
+
 #export data as tab delimited file
+z = write.table(z, "macrozooplankton.txt", sep="\t")
+
 
 
 #Egg data Set-----
